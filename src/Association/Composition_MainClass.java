43d8f943029ce4774399ed65106c3e44c9ef5891
Aneerudh <anee469@gmail.com>
2021-03-15 20:40:24 +0530

    changes

--- src/Association/Composition_MainClass.java
+++ src/Association/Composition_MainClass.java
@@ -2,7 +2,7 @@ package Association;
 
 public class Composition_MainClass {
     public static void main(String arg[]){
-        Account account = new Account("Subash A", 3527921);
+        Account account = new Account("Aneerudh", 3527921);
         account.showAlldetails();
     }
 }
--- src/socket/WhoIs.java
+++ src/socket/WhoIs.java
@@ -7,26 +7,28 @@ public class WhoIs {
 
 	public static void main(String[] args) throws Exception {
 		 
-        String domainName = "apple.com";
+        String domainName = "tce.edu";
  
-        String hostname = "whois.corporatedomains.com";//whois.educause.edu for tce.edu
-        int port = 43;
+        String hostname = "whois.educause.edu";//whois.educause.edu for tce.edu
+        int port = 43;// whois.corporatedomains.com for apple.com
  
         try (Socket socket = new Socket(hostname, port)) {
- 
+            // sending information to the whois server
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
             writer.println(domainName);
- 
+            
+            // getting the response from server and printitng it
             InputStream input = socket.getInputStream();
- 
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
  
             String line;
- 
+            FileWriter f = new FileWriter("Websitedata.txt");
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
+                f.write(line);
             }
+            f.close();
             reader.close();
         } catch (UnknownHostException ex) {
  
